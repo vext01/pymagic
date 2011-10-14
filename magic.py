fcd43aa1316b694e9d7c27eb0eb750c28787ee6c
vext01 <vext01@gmail.com>
2011-10-14 17:42:42 +0100

    this seems workable :)

--- magic.py
+++ magic.py
@@ -12,18 +12,21 @@ class node:
 
     def dump(self, level):
 
-        # sanity check
-        if (level != self.level):
-            print("failed sanity check")
-            sys.exit(1)
-
         sys.stdout.write("%s" % ("\t" * level))
         print(self)
         for i in self.children:
             i.dump(level+1)
 
+        # sanity check
+        if (level != self.level):
+            print("failed sanity check")
+            sys.exit(1)
+
+
+
     def __str__(self):
-        return "* [%s] (mime=%s)" % (self.descr, self.mime)
+        return "* [%s] (mime=%s, level=%d)" % \
+                (self.descr, self.mime, self.level)
 
 # get next non-blank, non-comment line
 def next_useful_line(f):
@@ -58,15 +61,15 @@ def attach_mime(f, node):
 """
 parse magic file making a tree
 """
-def parse(f, parent_node, limit):
+def parse(f, root_node):
 
     ignore = True
     raw_line = None
 
-    # keep reading until a useful line shows up
-    while (True and limit > 0):
+    last_node = root_node
 
-        limit -= 1 #XXX
+    # keep reading until a useful line shows up
+    while (True):
 
         try:
             raw_line = next_useful_line(f)
@@ -74,7 +77,6 @@ def parse(f, parent_node, limit):
             return # we are done
 
         line = raw_line.strip()
-        print(line)
 
         if line.startswith("!:mime"):
             print("Unexpected mime");
@@ -86,6 +88,7 @@ def parse(f, parent_node, limit):
             lvl += 1
             line = line[1:]
 
+        # construct the new node
         new_node = None
         flds = line.split(None, 3)
         flds = [ x for x in flds if len(x) != 0]
@@ -94,27 +97,39 @@ def parse(f, parent_node, limit):
         except(IndexError):
             new_node = node(lvl, "")
 
+        # attach mime iff a mime line follows
         attach_mime(f, new_node)
 
-        # XXX separate parent_node from last_node
-        if (new_node.level == parent_node.level + 1):
-            pass
-            # parent_node does not change
-        elif new_node.level == parent_node.level:
-            parent_node = parent_node.parent
-        elif new_node.level < parent_node.level:
-            for up in range (0, parent_node.level - new_node.level + 1):
-                parent_node = parent_node.parent
+        # decide where to insert the new node
+        insert_at = None
+        if (new_node.level == last_node.level):
+            # new node is on the same level as the last node's parent
+            # new parent becomes the last node's parent's parent
+            # (lol)
+            insert_at = last_node.parent
+
+        elif new_node.level == last_node.level + 1:
+            # new node on next level deep as last
+            # last node becomes new parent
+            insert_at = last_node
+
+        elif new_node.level < last_node.level:
+            # new node is further up the tree
+            # go up the difference in levels to find the new parent
+            insert_at = last_node.parent
+            for up in range (0, last_node.level - new_node.level):
+                insert_at = insert_at.parent
         else:
+            # if the new node has a level greater than 2 more than the last
+            # node, something went wrong.
             print("should never happen")
-            print("new_lvl = %d :: parent_lvl = %d" % (new_node.level,
-                parent_node.level))
-            # XXX some bad magic causes this. Hmmm
+            print("new_lvl = %d :: last_lvl = %d" % (new_node.level,
+                last_node.level))
             sys.exit(1)
 
-        new_node.parent = parent_node
-        parent_node.children.append(new_node)
-        parent_node = new_node
+        insert_at.children.append(new_node)
+        new_node.parent = insert_at
+        last_node = new_node
 
 """
 TEST --------------------------------------------------------------
@@ -122,17 +137,10 @@ TEST --------------------------------------------------------------
 
 magic = None
 if (len(sys.argv) == 1):
-    magic = open("/etc/magic", "r")
+    magic = open("magic", "r")
 else:
     magic = open(sys.argv[1], "r")
 
-sys.stdout.write("How many lines to parse (or hot enter for whole file: ")
-lim = raw_input()
-if len(lim.strip()) == 0:
-    lim = 99999999999
-else:
-    lim = int(lim)
-
 root = node(0, "<root>")
-parse(magic, root, lim)
+parse(magic, root)
 root.dump(0)
